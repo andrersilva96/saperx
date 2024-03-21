b8a7b82c4fb83299c145e47d2cbf579c6a9b5136
Andre Rodrigues <andre.rsilva96@hotmail.com>
2024-03-21 13:55:43 -0300

    Show users

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,22 @@
 import './bootstrap';
 
-import 'bootstrap';
+import * as bootstrap from 'bootstrap';
+
+window.bootstrap = bootstrap;
 
 import Alpine from 'alpinejs'
 
 Alpine.start()
 
 window.Alpine = Alpine
+
+// Active popovers from bootstrap 5
+if (parseInt(window.bootstrap.Tooltip.VERSION.charAt(0)) === 5) {
+    [...document.querySelectorAll('[data-bs-toggle="tooltip"]')].forEach(el => new bootstrap.Tooltip(el));
+    [...document.querySelectorAll('[data-bs-toggle="popover"]')].forEach(el => new bootstrap.Popover(el));
+
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+        return new bootstrap.Popover(popoverTriggerEl)
+    })
+}
--- resources/views/pages/users/show.blade.php
+++ resources/views/pages/users/show.blade.php
@@ -1,7 +1,45 @@
 @extends('layouts.guest')
 
 @section('content')
-    <h1 @click="alert(123)">hello world</h1>
+    <h1>Agenda Telefônica</h1>
+
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Nome</th>
+                <th scope="col">E-mail</th>
+                <th scope="col">CPF</th>
+                <th scope="col">Data de Nascimento</th>
+                <th scope="col">Telefones</th>
+                <th scope="col">Ações</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($users as $user)
+                <tr>
+                    <th scope="row">{{ $user->id }}</th>
+                    <td>{{ $user->name }}</td>
+                    <td>{{ $user->email }}</td>
+                    <td>{{ $user->doc }}</td>
+                    <td>{{ $user->birth_date }}</td>
+                    <td>
+                        {{ $user->phones->first() }}
+                        @if ($user->phones->count() != 1)
+                            <button class="btn btn-sm btn-primary" data-bs-toggle="tooltip" data-bs-placement="top"
+                                title="{{ $user->phones->implode(', ') }}">
+                                @include('icons.eye')
+                            </button>
+                        @endif
+                    </td>
+                    <td>
+                        <a href="#" class="btn btn-sm btn-success">Ver</a>
+                        <a href="#" class="btn btn-sm btn-danger ms-3">Excluir</a>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 @endsection
 
 @push('scripts')
